9ac6058dc08eee00003ef62d5ec33be70848cbf9
Piyush Pradhan <piyushpradhan3.14@gmail.com>
2024-10-08 20:35:47 +0530

    fix: revert changes made to vite config

--- apps/host/vite.config.ts
+++ apps/host/vite.config.ts
@@ -1,20 +1,10 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import federation from '@originjs/vite-plugin-federation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: 'host',
-      remotes: {
-        messaging: 'http://localhost:5174/assets/remoteEntry.js',
-      },
-      shared: ['react', 'react-dom'],
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
