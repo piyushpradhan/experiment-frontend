a49bb303b9fa2841dc23d5ddd744933908582da1
Piyush Pradhan <piyushpradhan3.14@gmail.com>
2024-11-12 20:55:36 +0530

    fix: remove dropdown menu

--- apps/host/src/pages/Home.tsx
+++ apps/host/src/pages/Home.tsx
@@ -94,26 +94,6 @@ const Home = () => {
                 </div>
               </form>
             </div>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button
-                  variant="secondary"
-                  size="icon"
-                  className="rounded-full"
-                >
-                  <CircleUser className="h-5 w-5" />
-                  <span className="sr-only">Toggle user menu</span>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Settings</DropdownMenuItem>
-                <DropdownMenuItem>Support</DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Logout</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
           </header>
           <main className="flex-1 overflow-y-auto">
             <Outlet />
